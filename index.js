139b5b61e2b45e01a5f4129e570d229385b0a07d
Your Name <new2282307660@gmail.com>
2021-04-24 11:36:06 -0700

    added workflow

---
+++ index.js
@@ -0,0 +1,23 @@
+require("isomorphic-unfetch");
+const { promises: fs } = require("fs");
+const path = require("path");
+
+async function main() {
+  const readmeTemplate = (
+    await fs.readFile(path.join(process.cwd(), "./README.template.md"))
+  ).toString("utf-8");
+
+  const quote = await (
+    await fetch("https://goquotes-api.herokuapp.com/api/v1/random?count=1")
+  ).json();
+
+  console.log(office_quote);
+
+  const readme = readmeTemplate
+    .replace("{office_quote}", quote.quotes[0].text)
+    .replace("{office_character}", `- ${oquote.quotes[0].author}`);
+
+  await fs.writeFile("README.md", readme);
+}
+
+main();
